f21d429538c730d750d48dc442f06bd957930964
William Do <william@ultimaspin.com>
2021-02-17 21:36:48 +0000

    fix kill for long pgid

--- kill.sh
+++ kill.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps xao pgid,comm | grep chromedriver | cut -d " " -f 2 | xargs -n1 -I {} sh -c "kill -- -{}"
+ps xao pgid,comm | grep chromedriver | sed -e 's/^[[:space:]]*//' | cut -d " " -f 1 | xargs -n1 -I {} sh -c "kill -- -{}"
