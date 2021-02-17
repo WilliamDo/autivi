e7be35942035faab9687ff81f6f6ef3aaca9dc38
William Do <william@ultimaspin.com>
2021-02-17 21:22:12 +0000

    Revert "use a python scheduler"
    This reverts commit d5dbf2366a94e721038dc95b17e776b24c3ed8aa.

--- autivi.py
+++ autivi.py
@@ -1,20 +1,9 @@
-import schedule
 import time
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 
+driver = Chrome()
+driver.get("http://www.vietchannels.com/watch2.php?id=434")
 
-def start_tivi():
-    with Chrome() as driver:
-        driver.get("http://www.vietchannels.com/watch2.php?id=434")
-
-        full_screen_button = driver.find_element(By.CSS_SELECTOR, "a.fp-fullscreen.fp-icon")
-        full_screen_button.click()
-        time.sleep(50400)
-
-schedule.every().day.at("08:00").do(start_tivi)
-
-while True:
-    schedule.run_pending()
-    print("started")
-    time.sleep(600)
+full_screen_button = driver.find_element(By.CSS_SELECTOR, "a.fp-fullscreen.fp-icon")
+full_screen_button.click()
