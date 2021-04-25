1beea63a43c99f8a20ce9c174c5d005ba0436326
William Do <william@ultimaspin.com>
2021-04-25 15:33:31 +0100

    add vtv vn channels for test

--- bativi.py
+++ bativi.py
@@ -5,33 +5,52 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 
 driver = Chrome()
+driver.implicitly_wait(10)
 presses = Queue(maxsize=1)
+
+vtvgo = {
+    '1': "https://vtvgo.vn/xem-truc-tuyen-kenh-vtv1-1.html",
+    '2': "https://vtvgo.vn/xem-truc-tuyen-kenh-vtv2-2.html",
+    '3': "https://vtvgo.vn/xem-truc-tuyen-kenh-vtv3-3.html",
+    '4': "https://vtvgo.vn/xem-truc-tuyen-kenh-vtv4-4.html",
+    '5': "https://vtvgo.vn/xem-truc-tuyen-kenh-vtv5-5.html",
+    '6': "https://vtvgo.vn/xem-truc-tuyen-kenh-vtv6-6.html",
+    '7': "https://vtvgo.vn/xem-truc-tuyen-kenh-vtv7-27.html",
+    '8': "https://vtvgo.vn/xem-truc-tuyen-kenh-vtv8-36.html"
+}
+
+vtvvn = {
+
+    '1': "https://vtv.vn/truyen-hinh-truc-tuyen/vtv1.htm",
+    '2': "https://vtv.vn/truyen-hinh-truc-tuyen/vtv2.htm",
+    '3': "https://vtv.vn/truyen-hinh-truc-tuyen/vtv3.htm",
+    '4': "https://vtv.vn/truyen-hinh-truc-tuyen/vtv4.htm",
+    '5': "https://vtv.vn/truyen-hinh-truc-tuyen/vtv5.htm",
+    '6': "https://vtv.vn/vtv6/truyen-hinh-truc-tuyen.htm",
+    '7': "https://vtv.vn/truyen-hinh-truc-tuyen/vtv7.htm",
+    '8': "https://vtv.vn/vtv8/truyen-hinh-truc-tuyen.htm",
+    '9': "https://vtv.vn/truyen-hinh-truc-tuyen/vtv9.htm"
+}
 
 def worker():
     while True:
         key = presses.get()
-        print(f"worker {key}")
-        if key.char == '1':
-            play_vtvgo("https://vtvgo.vn/xem-truc-tuyen-kenh-vtv1-1.html")
-        elif key.char == '2':
-            play_vtvgo("https://vtvgo.vn/xem-truc-tuyen-kenh-vtv2-2.html")
-        elif key.char == '3':
-            play_vtvgo("https://vtvgo.vn/xem-truc-tuyen-kenh-vtv3-3.html")
-        elif key.char == '4':
-            play_vtvgo("https://vtvgo.vn/xem-truc-tuyen-kenh-vtv4-4.html")
+        if key.char in vtvgo:
+            print(f"worker {key}")
+            play_vtv(vtvgo[key.char])
         else:
             print(key.char)
 
 Thread(target=worker, daemon=True).start()
 
-def play_vtvgo(url):
+def play_vtv(url):
     driver.get(url)
     
     play_button = driver.find_element(By.CSS_SELECTOR, "button.vjs-play-control")
     play_button.click()
 
     full_screen_button = driver.find_element(By.CSS_SELECTOR, "button.vjs-fullscreen-control")
-    #full_screen_button.click()
+    full_screen_button.click()
 
 def on_press(key):       
     try:
